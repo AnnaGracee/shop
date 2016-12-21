f82f547a8cbac0d7e2073db3ee89b28f323abb07
AnnaGracee <ann86reznik@gmail.com>
2016-12-21 23:43:12 +0100

    First commit

---
+++ views/site/about.php
@@ -0,0 +1,34 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-6">
+                <h4>O nas</h4>
+
+                <br/>
+
+                               
+                
+                <p>Dzień dobry.
+
+                <p>Nasz sklep rozpoczął swoją działalność w 2016 roku i mamy nadzieję,
+                   zyska wielkie uznanie wśród konsumentów.
+                <p>Zajmujęmy się sprzedażą hurtową oraz detaliczną luksusowych 
+                   akcesoriów telefonicznych, oferując najbogatszą ofertę 
+                   produktów na polskim rynku.Obecnie asortyment obejmuje szeroką 
+                   gamę etui i futerałów dedykowanych dla różnych telefonów i tabletów.
+                   W ofercie można także znaleźć wszelkiego rodzaju akcesoria: 
+                   folie ochronne, uchwyty samochodowe, ładowarki czy rysiki.
+                <p>Naszym celem jest tworzenie idealniej równowagi między formą,
+                   funkcjonalnością a poziomem ochrony poprzez wyszukiwanie innowacyjnych
+                   konstrukcji i ciekawych materiałów produkcyjnych.
+                <p>Zapraszamy do współpracy.
+                <p>Pozdrawiamy Anna i Vlad.
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
